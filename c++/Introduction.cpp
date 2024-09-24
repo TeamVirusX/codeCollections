570ff559fb379d356d2fb397b6b4815ae01b8d92
KeithJP19 <150800104+KeithJP19@users.noreply.github.com>
2024-09-24 10:10:36 +0800

    Add files via upload
    Basta itry mo nalang :>>
    Just try it :>>

---
+++ c++/Introduction.cpp
@@ -0,0 +1,15 @@
+//Use C++ to compile and run this code!
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	string name;
+	cout << "Enter your Name: ";
+	cin >> name;
+	cout << "Hello " << name << "! Welcome!";
+	
+
+
+    return 0;
+}
