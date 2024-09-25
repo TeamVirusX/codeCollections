1f0ddd249df1639e1585f477bb263ce114f9ce54
Kiruu <23ln5567_ms@psu.edu.ph>
2024-09-25 12:26:06 +0800

    Create TheKiruuWay.java

---
+++ java/Assignment-Basic-Calculator/TheKiruuWay.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class TheKiruuWay {
+    public static void main (String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        // ARRAYYY
+        String[] operationResult= {"sum", "difference", "product", "quotient", "remainder"};
+        // Considering that we are making a CLI-based program.
+        System.out.println("Choose the operation\n1. Addition\n2. Subtraction\n3. Multiplication \n4. Division \n5. Modulo");
+        int operation = myScanner.nextInt();
+        // Using ternary operator for conditio testing
+        System.out.println((operation > 5 || operation < 1) ? "Invalid operator!" : "");
+        // Getting the input for 2 operands
+        System.out.print("Enter the first operand: ");
+        float x = myScanner.nextFloat();
+        System.out.print("Enter the second operand: ");
+        float y = myScanner.nextFloat();
+        // Calculate the result by invoking the calculate function
+        float total = calculate(x, y, operation);
+        System.out.println("The " + operationResult[operation - 1] + " of " + x + " and " + y + " is " + total);
+    }
+    public static float calculate(float firstOperand, float secondOperand, int operation) {
+        switch (operation) {
+            case 1:
+                return firstOperand + secondOperand;
+            case 2:
+                return firstOperand - secondOperand;
+            case 3:
+                return firstOperand * secondOperand;
+            case 4:
+                return firstOperand / secondOperand;
+            case 5:
+                return firstOperand % secondOperand;
+        }
+         return -1; // tamad ako eh
+    }
+}
