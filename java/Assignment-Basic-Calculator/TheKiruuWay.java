660827f7daccc6d55e8a55173699c1ff36ad0754
Kiruu <23ln5567_ms@psu.edu.ph>
2024-10-01 20:40:15 +0800

    Created quotient error handling

--- java/Assignment-Basic-Calculator/TheKiruuWay.java
+++ java/Assignment-Basic-Calculator/TheKiruuWay.java
@@ -1,28 +1,28 @@
-// @iAmK1ruu
+
 import java.util.Scanner;
 public class TheKiruuWay {
     public static void main (String[] args){
         Scanner myScanner = new Scanner(System.in);
-        // ARRAYYY
         String[] operationResult= {"sum", "difference", "product", "quotient", "remainder"};
-        // Considering that we are making a CLI-based program.
         System.out.println("Choose the operation\n1. Addition\n2. Subtraction\n3. Multiplication \n4. Division \n5. Modulo");
         int operation = myScanner.nextInt();
         if ((operation > 5 || operation < 1)) {
             System.out.println("Invalid operation!");
             main(args);
         } else {
-        // Getting the input for 2 operands
         System.out.print("Enter the first operand: ");
         float x = myScanner.nextFloat();
         System.out.print("Enter the second operand: ");
         float y = myScanner.nextFloat();
-        // Calculate the result by invoking the calculate function
+        if((operation == 4 && (x > 0 && y == 0)) || (operation == 4 && (x == 0 && y == 0))){
+            System.out.println("Invalid quotient!");
+            System.exit(0);
+        }
         float total = calculate(x, y, operation);
         myScanner.close();
         System.out.println("The " + operationResult[operation - 1] + " of " + x + " and " + y + " is " + total);
         }
-    
+    }
     public static float calculate(float firstOperand, float secondOperand, int operation) {
         return switch (operation) {
             case 1 -> firstOperand + secondOperand;
