038e1541dfdea8158031c80a475430b521420e87
KeithJP19 <150800104+KeithJP19@users.noreply.github.com>
2024-10-04 10:07:43 +0800

    Create Using_a_String.cpp
    basta ganyan gumamit ng string

---
+++ c++/Using_a_String.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+    string name;
+    cout << "Please Enter your Name: " ;
+    getline(cin,name);
+    cout << "Welcome! " << rivuen;
+    
+    
+    return 0;
+}
