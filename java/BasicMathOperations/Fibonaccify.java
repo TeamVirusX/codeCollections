53e8edd1809746a92c985e23cbee95e21194d69c
Kiruu <23ln5567_ms@psu.edu.ph>
2024-10-15 19:54:51 +0800

    Create Fibonaccify.java

---
+++ java/BasicMathOperations/Fibonaccify.java
@@ -0,0 +1,17 @@
+// Using binet's formula instead of recursion and array-type implementation
+import java.util.Scanner;
+public class Fibonaccify {
+    public double Fibonacci(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        return (Math.pow(phi, n) - Math.pow(1 - phi, n)) / Math.sqrt(5);
+    }
+    public static void main(String[] args) {
+        Fibonaccify fib = new Fibonaccify();
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Fibonaccify Me!\nEnter a number to get the Fibonacci sequence of up to nth term:");
+        int n = myScanner.nextInt();
+        for (int i = 0 ; i < n; i++) {
+            System.out.println((int)fib.Fibonacci(i));
+        }
+    }
+}
