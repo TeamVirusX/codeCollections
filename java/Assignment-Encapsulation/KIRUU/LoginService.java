f93efac5dcc9519855c8551ec8c7be3f12be5f1e
Kiruu <23ln5567_ms@psu.edu.ph>
2024-11-22 17:48:04 +0800

    Update LoginService.java

--- java/Assignment-Encapsulation/KIRUU/LoginService.java
+++ java/Assignment-Encapsulation/KIRUU/LoginService.java
@@ -1,7 +1,6 @@
 // THIS IS SIMPLIFIED TO AN EXTENT
 import java.util.Scanner;
 public class LoginService {
-    private int loginCount = 0; 
     public boolean login(SystemUser user) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter password:");
